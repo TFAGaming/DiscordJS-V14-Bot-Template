c407ca009587242c6425328e206cba00018068f8
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2022-07-19 12:31:33 +0100

    📄 New file: owner.js

--- commands/General/owner.js
+++ commands/General/owner.js
@@ -5,10 +5,21 @@ module.exports = {
   permissions: ['SendMessages'],
   owner: true, // If this is "true", only owners that were provided in config.json are allowed to use this command. If this is "false" or not added, everyone can use it and also must have the required permission(s) above.
   run: async (client, message, args, prefix, config) => {
+    
+    const ownersID = config.Users.OWNERS;
+    if (!ownersID) return;
+    
+    const ownersARRAY = [];
+    
+    ownersID.forEach(Owner => {
+      const fetchedOWNER = message.guild.members.cache.get(Owner);
+      if (!fetchedOWNER) ownersARRAY.push("*Unknown User#0000*");
+      ownersARRAY.push(`${fetchedOWNER.user.tag}`);
+    });
 
     message.reply({ embeds: [
       new EmbedBuilder()
-        .setDescription("Only owner command! :)")
+        .setDescription(`Only owners command! \nOwners: **{owners.join(", ")}**`)
         .setColor("Yellow")
     ] })
     
