3cb0eb93f15ea824fac6a97b0523dd04f1ca124e
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2022-07-29 15:45:57 +0100

    🔩 Add custom prefix system
    Using quick.db

--- events/Guild/messageCreate.js
+++ events/Guild/messageCreate.js
@@ -1,13 +1,16 @@
 const { EmbedBuilder, PermissionsBitField } = require("discord.js");
 const client = require("../../index");
 const config = require("../../config/config.json");
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
 
 module.exports = {
   name: "messageCreate"
 };
 
 client.on('messageCreate', async message => {
-  const prefix = config.Prefix || "?";
+  
+  const prefix = await db.get(`guild_prefix_${message.guild.id}`) || config.Prefix || "?";
 
   if (message.channel.type !== 0) return;
   if (message.author.bot) return;
@@ -54,6 +57,6 @@ client.on('messageCreate', async message => {
       })
     }
 
-    command.run(client, message, args, prefix, config);
+    command.run(client, message, args, prefix, config, db);
   }
 })
