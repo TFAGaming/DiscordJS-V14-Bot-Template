d3c8ecd0ff65cfb6c30026a5a61a2e7a0b0c9d0b
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2022-08-03 12:45:38 +0100

    Create ping.js

---
+++ slashCommands/General/ping.js
@@ -0,0 +1,14 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  name: "ping",
+  description: "Return websocket ping",
+  run: async (client, interaction, config, db) => {
+    
+    interaction.reply({ embeds: [
+      new EmbedBuilder()
+        .setDescription(`${client.ws.ping} ms!`)
+    ] })
+    
+  }  
+}
