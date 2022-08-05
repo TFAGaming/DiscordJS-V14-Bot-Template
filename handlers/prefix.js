26677363852ed9fbba34b4f290950ceb9ecfb6e2
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2022-08-05 17:12:00 +0100

    ☑️ Updated the prefix commands

--- handlers/prefix.js
+++ handlers/prefix.js
@@ -3,20 +3,20 @@ const colors = require("colors");
 
 module.exports = (client) => {
   console.log("0------------------| Prefix Handler:".blue);
-  
+
   fs.readdirSync('./commands/').forEach(dir => {
-		const commands = fs.readdirSync(`./commands/${dir}`).filter(file => file.endsWith('.js'));
-		for (let file of commands) {
-      
-			let pull = require(`../commands/${dir}/${file}`);
-			if (pull.name) {
-				client.commands.set(pull.name, pull);
-				console.log(`[HANDLER - PREFIX] Loaded a file: ${pull.name} (#${client.commands.size})`.brightGreen)
-			} else {
-				console.log(`[HANDLER - PREFIX] Couldn't load the file ${file}, missing module name value.`.red)
-				continue;
-			}
-      
-		}
-	});
+    const commands = fs.readdirSync(`./commands/${dir}`).filter(file => file.endsWith('.js'));
+    for (let file of commands) {
+
+      let pull = require(`../commands/${dir}/${file}`);
+      if (pull.config.name) {
+        client.commands.set(pull.config.name, pull);
+        console.log(`[HANDLER - PREFIX] Loaded a file: ${pull.config.name} (#${client.commands.size})`.brightGreen)
+      } else {
+        console.log(`[HANDLER - PREFIX] Couldn't load the file ${file}, missing module name value.`.red)
+        continue;
+      }
+
+    }
+  });
 }
