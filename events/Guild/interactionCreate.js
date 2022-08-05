4c45b6502043ee137fba423eb97abca339f50f95
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2022-08-05 16:52:23 +0100

    🦾 Better slash commands handler

--- events/Guild/interactionCreate.js
+++ events/Guild/interactionCreate.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, PermissionsBitField, InteractionType } = require("discord.js");
+const { EmbedBuilder, PermissionsBitField } = require("discord.js");
 const client = require("../../index");
 const config = require("../../config/config.json");
 const { QuickDB } = require("quick.db");
@@ -9,17 +9,38 @@ module.exports = {
 };
 
 client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+  if (!interaction.type == 2) return;
+
   const command = client.slashcmds.get(interaction.commandName);
 
   if (!command) return;
 
-  if (interaction.type === InteractionType.ApplicationCommand) {
-    try {
-      await command.run(client, interaction, config, db);
-    } catch (error) {
-      console.log(error);
-    }
-  } else {
-    interaction.reply({ content: "An Error has occured.", ephemeral: true }).catch(() => { });
+  try {
+    if (command.guild_member_permissions) {
+      if (!interaction.memberPermissions.has(PermissionsBitField.resolve(command.guild_member_permissions || []))) return interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setDescription(`🚫 Unfortunately, you are not authorized to use this command.`)
+            .setColor("Red")
+        ],
+        ephemeral: true
+      });
+    } else if (command.guild_client_permissions) {
+      if (!interaction.guild.members.cache.get(client.user.id).permissions.has(PermissionsBitField.resolve(command.guild_client_permissions || []))) return interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setDescription(`🚫 Unfortunately, I can't execute this command.`)
+            .setFooter({ text: `Required permissions: ${command.guild_client_permissions.join(", ")}` })
+            .setColor("Red")
+        ],
+        ephemeral: true
+      });
+    };
+
+    await command.run(client, interaction, config, db);
+
+  } catch (error) {
+    console.log(error);
   }
 });
