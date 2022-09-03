7589919bfc07ee9619eed34ca79094c169ab265e
๖ۣۜŇŇ_MyTツ <myt@nonamestudio.xyz>
2022-09-03 11:55:41 +0700

    Update ready.js
    Make an example for changing a bot's status or its presence.

--- events/Client/ready.js
+++ events/Client/ready.js
@@ -7,4 +7,12 @@ module.exports = {
 
 client.once('ready', async () => {
   console.log("\n" + `[READY] ${client.user.tag} is up and ready to go.`.brightGreen);
+  
+  client.user.setPresence({
+        status: "online"
+    }); //change bot's presence to online
+  
+  client.user.setActivity(`Chilling on ${client.guilds.cache.size} servers`, {
+            type: "WATCHING"
+   }); // Set a custom status for your bot.
 })
