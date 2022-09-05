269449441bed6579769f91ef7a262017657b644b
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2022-09-05 10:56:13 +0100

    😎 V1.5

---
+++ config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  Prefix: "?",
+
+  Users: {
+    OWNERS: ["849413565487382578", "554029264492691476"]
+  },
+
+  Handlers: {
+    MONGO: ""
+  },
+
+  Client: {
+    TOKEN: "OTI3MjI2Nzg3MTc0NDE2NDY0.GEoC3P.NLaD33LWEmbR-dKQFuyxgUePwOhmVdrz0Ilm8k",
+    ID: "927226787174416464"
+  }
+
+}
