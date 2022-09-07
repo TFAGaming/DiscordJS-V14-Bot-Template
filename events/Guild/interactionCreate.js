89e6d4d576f98a925a4b33c6a16bbf8344f78e69
Kane <50002439+kanetjuh@users.noreply.github.com>
2022-09-07 17:47:20 +0200

    Updated interactionCreate.js
    It was missing the event for `// Message:`

--- events/Guild/interactionCreate.js
+++ events/Guild/interactionCreate.js
@@ -32,6 +32,18 @@ client.on('interactionCreate', async (interaction) => {
       console.error(e)
     };
   };
+  
+  if (interaction.isMessageContextMenuCommand()) { // Message:
+    const command = client.message_commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+      command.run(client, interaction, config, db);
+    } catch (e) {
+      console.error(e)
+    };
+  };
 
   if (interaction.isModalSubmit()) { // Modals:
     const modal = client.modals.get(interaction.customId);
