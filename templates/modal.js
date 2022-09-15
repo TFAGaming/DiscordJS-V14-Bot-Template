81991b2671e50e73cd70ff7f4e2fde3191b16f0c
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2022-09-15 18:31:53 +0100

    Modal template added.

---
+++ templates/modal.js
@@ -0,0 +1,10 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    id: "", // Your modal customId.
+    run: async (client, interaction, config, db) => {
+
+        // execute.
+
+    },
+};
