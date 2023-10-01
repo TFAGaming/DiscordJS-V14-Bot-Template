27f6f132177f8b61865b167441afc52af6b63d8f
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-10-01 14:26:07 +0600

    Use info level on the load command instead of warning

--- src/handlers/deploy.js
+++ src/handlers/deploy.js
@@ -1,24 +1,32 @@
 const { REST, Routes } = require("discord.js");
 const { log } = require("../functions");
 const config = require("../config");
-const ExtendedClient = require('../class/ExtendedClient');
+const ExtendedClient = require("../class/ExtendedClient");
 
 /**
- * 
- * @param {ExtendedClient} client 
+ *
+ * @param {ExtendedClient} client
  */
 module.exports = async (client) => {
-    const rest = new REST({ version: '10' }).setToken(process.env.CLIENT_TOKEN || config.client.token);
+  const rest = new REST({ version: "10" }).setToken(
+    process.env.CLIENT_TOKEN || config.client.token
+  );
 
-    try {
-        log('Started loading application commands... (this might take minutes!)', 'warn');
+  try {
+    log(
+      "Started loading application commands... (this might take minutes!)",
+      "info"
+    );
 
-        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID || config.client.id), {
-            body: client.applicationcommandsArray
-        });
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID || config.client.id),
+      {
+        body: client.applicationcommandsArray,
+      }
+    );
 
-        log('Successfully loaded application commands to Discord API.', 'done');
-    } catch (e) {
-        log('Unable to load application commands to Discord API.', 'err');
-    };
+    log("Successfully loaded application commands to Discord API.", "done");
+  } catch (e) {
+    log("Unable to load application commands to Discord API.", "err");
+  }
 };
